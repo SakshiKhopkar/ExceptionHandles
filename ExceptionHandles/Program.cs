aadc0f8adc0c3d53a660f72f6873a1b49b9b5dac
Sakshi <Sakshi@LAPTOP-UQ1QRO8A>
2023-09-13 18:32:56 +0530

    Add project files.

---
+++ ExceptionHandles/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Name ");
+            try
+            {
+                string name = Console.ReadLine();
+                //string name = null;
+                
+                if(string.IsNullOrEmpty(name))
+                {
+                    throw new Exception("Name is required");
+                }
+                Console.WriteLine(name);
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            finally { Console.WriteLine("Inside finally block"); }
+
+        }
+    }
+}
---
+++ ExceptionHandles/RunNameExeception.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandles
+{
+    public class NameException : Exception
+    {
+        public NameException(string error):base(error) 
+        { 
+
+        }
+    }
+    public class Student
+    {
+        public Student(string name)
+        {
+           if(string.IsNullOrEmpty(name))
+           {
+                throw new NameException("string is empty");
+           }
+        }
+            
+
+    }
+
+    internal class RunNameExeception
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Student s1 = new Student("");
+            }
+            catch (NameException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
